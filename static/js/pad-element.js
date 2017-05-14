4c6a9c8ffa43e4544767b42673114cd59f2bd651
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2017-05-14 18:06:15 +0100

    Add modular JS, will compile for nomodule later.

---
+++ static/js/pad-element.js
@@ -0,0 +1,58 @@
+import { audioContext } from './audio-context.js';
+
+
+export class CustomPadElement extends HTMLElement {
+  constructor() {
+    super();
+    this.onInputStart = this.onInputStart.bind(this);
+    this.onInputStop = this.onInputStop.bind(this);
+  }
+
+  set active(bool) {
+    this.classList.toggle('mpc__pad--active', bool);
+  }
+
+  get key() {
+    return this.getAttribute('key');
+  }
+
+  connectedCallback() {
+    this.loadSample();
+    this.addEventListener('mousedown', this.onInputStart);
+    this.addEventListener('mouseup', this.onInputStop);
+    this.addEventListener('touchstart', this.onInputStart);
+    this.addEventListener('touchend', this.onInputStop);
+  }
+
+  disconnectedCallback() {
+    this.removeEventListener('mousedown', this.onInputStart);
+    this.removeEventListener('mouseup', this.onInputStop);
+    this.removeEventListener('touchstart', this.onInputStart);
+    this.removeEventListener('touchend', this.onInputStop);
+  }
+
+  onInputStart() {
+    const source = audioContext.createBufferSource();
+    source.connect(audioContext.destination);
+    source.buffer = this.sample;
+    this.active = true;
+    source.start();
+  }
+
+  onInputStop() {
+    this.active = false;
+
+  }
+
+  loadSample() {
+    const sample = this.getAttribute('sample');
+    const url = `../audio/${sample}.wav`;
+    fetch(url).then(response => {
+      return response.arrayBuffer();
+    }).then(buffer => {
+      return audioContext.decodeAudioData(buffer);
+    }).then(sample => {
+      this.sample = sample;
+    })
+  }
+}
